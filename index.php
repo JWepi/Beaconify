6a2a75bcd50950ad57d48c0f9de633dfffce5dc3
JWepi <david.lassagne@gmail.com>
2014-09-30 23:34:19 +0200

    Website base

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr"> 
+	<head> 
+		<title>Beaconify</title> 
+		
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+		<meta name="description" content="Beaconify" />
+		<meta name="keywords" content="beaconify javascript html css JW lassagne"/>
+		<meta name="author" content="JackW" />
+		<meta name="category" content="Website" />
+		<meta http-equiv="Content-Language" content="en-US" />
+		
+		<link rel="stylesheet" type="text/css" title="style" href="css/style.css" />
+	</head>
+	
+	<body>
+	
+	</body>
+</html>
+
+<script src="js/libs/jquery.com/jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="js/scsbase.js"></script>
+
+<script type="text/javascript">
+
+	$( document ).ready(function() {
+		console.log( "ready!" );
+		InitApp();
+	});
+
+</script>
---
+++ js/scsbase.js
@@ -0,0 +1,5 @@
+
+var InitApp = function()
+{
+	alert("init !");
+};
